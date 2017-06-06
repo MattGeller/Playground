b3f9d5ea850b1c0d45314bad1ea273b3bee4301e
Matt Geller <MattG92@gmail.com>
2017-06-05 21:56:45 -0700

    figured out more for loops!

--- PlaygroundFile.py
+++ PlaygroundFile.py
@@ -1,4 +1,5 @@
-print("Hello, world")
+my_string = "Hello, world"
+print(my_string)
 
 
 def string_bits(str):
@@ -6,3 +7,11 @@ def string_bits(str):
     for i in range(0, len(str), 2):
         temp = temp + str[i]
     return temp
+
+def string_splosion(str):
+  temp = ""
+  for i in range(0, len(str)):
+    temp = temp + str[0:i+1]
+  return temp
+
+print(string_splosion(my_string))
